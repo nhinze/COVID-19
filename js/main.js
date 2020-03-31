3664284161d0e42e60797b3ac388218c4b75e01f
Nicolas Hinze <findernetworkllc@gmail.com>
2020-03-31 10:14:28 -0400

    Add more plots

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var charts = [];
 
 var positives = [];
+var hospitalized = [];
 var deaths = [];
+var tests = [];
 
 var population = {};
 
@@ -83,15 +85,28 @@ function getData(state_1, state_2) {
     positives.y1 = [];
     positives.y2 = [];
 
+    hospitalized.x1 = [];
+    hospitalized.x2 = [];
+    hospitalized.y1 = [];
+    hospitalized.y2 = [];
+
     deaths.x1 = [];
     deaths.x2 = [];
     deaths.y1 = [];
     deaths.y2 = [];
 
+    tests.x1 = [];
+    tests.x2 = [];
+    tests.y1 = [];
+    tests.y2 = [];
+
     $.getJSON( "https://covidtracking.com/api/states/daily?state=" + state_1, function( data ) {
 
         var positive_counter = 0;
+        var hospital_counter = 0;
         var death_counter = 0;
+        var test_counter = 0;
+
         var state_pop = getPopulation(state_1);
 
         for (var i = 0; i < data.length; i++) {
@@ -102,11 +117,25 @@ function getData(state_1, state_2) {
                 positive_counter += 1
             }
 
+            var hospital = data[data.length - i - 1].hospitalized;
+            if (hospital > 0) {
+                hospitalized.x1.push(hospital_counter);
+                hospitalized.y1.push(Math.round(hospital / state_pop));
+                hospital_counter += 1
+            }
+
             var death = data[data.length - i - 1].death;
             if (death > 0) {
                 deaths.x1.push(death_counter);
                 deaths.y1.push(Math.round(death / state_pop));
                 death_counter += 1;
+            }
+
+            var test = data[data.length - i - 1].totalTestResults;
+            if (test > 0) {
+                tests.x1.push(test_counter);
+                tests.y1.push(Math.round(test / state_pop));
+                test_counter += 1;
             }
         }
 
@@ -116,7 +145,10 @@ function getData(state_1, state_2) {
     $.getJSON( "https://covidtracking.com/api/states/daily?state=" + state_2, function( data ) {
 
         var positive_counter = 0;
+        var hospital_counter = 0;
         var death_counter = 0;
+        var test_counter = 0;
+
         var state_pop = getPopulation(state_2);
 
         for (var i = 0; i < data.length; i++) {
@@ -127,11 +159,25 @@ function getData(state_1, state_2) {
                 positive_counter += 1
             }
 
+            var hospital = data[data.length - i - 1].hospitalized;
+            if (hospital > 0) {
+                hospitalized.x2.push(hospital_counter);
+                hospitalized.y2.push(Math.round(hospital / state_pop));
+                hospital_counter += 1
+            }
+
             var death = data[data.length - i - 1].death;
             if (death > 0) {
                 deaths.x2.push(death_counter);
                 deaths.y2.push(Math.round(death / state_pop));
                 death_counter += 1;
+            }
+
+            var test = data[data.length - i - 1].totalTestResults;
+            if (test > 0) {
+                tests.x2.push(test_counter);
+                tests.y2.push(Math.round(test / state_pop));
+                test_counter += 1;
             }
         }
 
@@ -152,6 +198,16 @@ function check_ready(state_1, state_2) {
         plot('casesChart', labels, positives.y1, positives.y2, state_1, state_2, 'Covid-19 cases per 1M', 'Days since first Covid-19 case', 'Covid-19 cases per 1M');
     }
 
+    if (hospitalized.y1.length > 0 && hospitalized.y2.length > 0) {
+        var labels;
+        if (hospitalized.x1.length > hospitalized.x2.length) {
+            labels = hospitalized.x1;
+        } else {
+            labels = hospitalized.x2;
+        }
+        plot('hospitalizedChart', labels, hospitalized.y1, hospitalized.y2, state_1, state_2, 'Covid-19 hospitalizations per 1M', 'Days since first Covid-19 hospitalization', 'Covid-19 hospitalizations per 1M');
+    }
+
     if (deaths.y1.length > 0 && deaths.y2.length > 0) {
         var labels;
         if (deaths.x1.length > deaths.x2.length) {
@@ -160,6 +216,16 @@ function check_ready(state_1, state_2) {
             labels = deaths.x2;
         }
         plot('deathsChart', labels, deaths.y1, deaths.y2, state_1, state_2, 'Covid-19 deaths per 1M', 'Days since first Covid-19 death', 'Covid-19 deaths per 1M');
+    }
+
+    if (tests.y1.length > 0 && tests.y2.length > 0) {
+        var labels;
+        if (tests.x1.length > tests.x2.length) {
+            labels = tests.x1;
+        } else {
+            labels = tests.x2;
+        }
+        plot('testsChart', labels, tests.y1, tests.y2, state_1, state_2, 'Covid-19 tests per 1M', 'Days since first Covid-19 test', 'Covid-19 tests per 1M');
     }
 
 }
